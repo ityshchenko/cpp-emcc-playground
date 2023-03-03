1c8398dc49c354f3070f2b25df0478e1bc698b38
Ivan Tyshchenko <5057658+nohitmai@users.noreply.github.com>
2023-03-04 00:28:31 +0200

    Add hello world with Actions CI preset for em++

---
+++ README.md
@@ -0,0 +1,2 @@
+# cpp-emcc-playground
+Example of the repo with preconfigured Actions CI for C++ simple students console projects.
---
+++ src/lesson_0/task.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+    std::string name;
+
+    std::cout << "Input your name: ";
+    std::cin >> name;
+    std::cout << "Hello, " << name << std::endl;
+
+    return 0;
+}
